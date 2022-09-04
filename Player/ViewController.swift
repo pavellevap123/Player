510cdc1b459b87a80d069ce78d5e16647cf2003a
Pavel Poddubotskiy <pavel.poddubotskiy@pm.by>
2022-09-04 13:23:07 +0300

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by Pavel Poddubotskiy on 4.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
